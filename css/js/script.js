4155a8cef68aee7452a174997ed81b95bd134290
sachingaud870-cmd <sachingaud870@gmail.com>
2026-01-13 15:24:20 +0530

    Add files via upload
    Add CSS JS and images

---
+++ css/js/script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const nav = document.getElementById("navbar");
+    nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+}
+function enquirySuccess() {
+    alert("Your enquiry has been sent successfully!");
+}
